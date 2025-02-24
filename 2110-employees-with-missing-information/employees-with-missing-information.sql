39785d28a84dc68afa31004837d7f30615deab7e
Satyanarayana Vinay Achanta <160984161+svinayachanta@users.noreply.github.com>
2025-02-24 13:53:56 -0700

    Time: 974 ms (21.12%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2110-employees-with-missing-information/employees-with-missing-information.sql
@@ -0,0 +1,5 @@
+select isnull(e.employee_id, s.employee_id) as employee_id
+from employees e
+full outer join salaries s on e.employee_id = s.employee_id
+where e.employee_id is null or s.employee_id is null
+order by employee_id
