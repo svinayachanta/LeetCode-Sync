bd45f12173606da667108ad2463a06566d308f1d
Satyanarayana Vinay Achanta <160984161+svinayachanta@users.noreply.github.com>
2025-02-24 14:40:21 -0700

    Time: 1825 ms (60.99%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1625-group-sold-products-by-the-date/group-sold-products-by-the-date.sql
@@ -0,0 +1,5 @@
+select distinct sell_date, count(distinct product) as num_sold, string_agg(product,',') as products
+from (select distinct *
+    from Activities) t
+group by sell_date 
+order by sell_date
